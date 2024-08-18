47934132a19aa3c1400f3e9181332e350f2d5c44
Takrimul Alam Chowdhury <164798428+Takrimalam@users.noreply.github.com>
2024-08-18 16:21:28 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+/*ex1*/
+function showtext(){
+	document.getElementById('s_text').innerHTML="Hello SSB";
+}
+
+/*ex2*/
+function showdate(){
+	document.getElementById('s_date').innerHTML=Date();
+}
+
+/*ex3*/
+function bulbOn(){
+	document.getElementById('img').src="images/on.gif";
+}
+
+function bulbOff(){
+	document.getElementById('img').src="images/off.gif";
+}
+
+/*ex4*/
+function changecolor(){
+	document.getElementById('css').style.color="red";
+}
+
+/*ex5*/
+function changefont(){
+	document.getElementById('font').style.fontSize="70px";
+}
+
+/*ex6*/
+function showcontent(){
+	document.getElementById('con').style.display="block";
+}
+function hidecontent(){
+	document.getElementById('con').style.display="none";
+}
+
+/*ex6*/
+function bulbOnO1(){
+    document.getElementById('imag').src='images/on.gif';
+} 
+function bulbOff2(){
+    document.getElementById('imag').src='images/off.gif';
+}
